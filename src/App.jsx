182767c8a4b055a0e2edd1aa05791ac052ca245c
my project <iconfavour005@email.com>
2024-02-17 23:22:01 -0800

    added speed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Testimonials, CaseStudies, Contact, Footer, Hero, Process, Services, Team , CTA} from './container';
 import { Menu } from './components';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
  
+injectSpeedInsights();
 inject();
+
 
 
 
